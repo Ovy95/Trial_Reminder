07323882d2872b4c5166afe01063bf55c4af6160
Ovy95 <jackoverton21@hotmail.com>
2020-05-17 14:30:48 +0100

    Can send emails using a timer scheduler

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 npm install 
  npm install --save express body-parser express-handlebars nodemailer
+ npm install --save node-cron
 This is like rackup
  npm install nodemon
--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const path = require('path')
+
 const nodemailer = require('nodemailer');
+let cron = require('node-cron');
 
 const app = express();
 
@@ -49,7 +51,7 @@ app.post('/send', (req, res) => {
   // Just checks the email
   console.log(email)
     // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
+    let transporter = nodemailer.createTransport({ 
       
       // THis domain name 
       host: "smtp.gmail.com",
@@ -57,7 +59,8 @@ app.post('/send', (req, res) => {
       secure: false, // true for 465, false for other ports
       // service: 'gmail',
       auth: {
-        // Added this to git without email and password loggin needed
+        user: , // generated ethereal user
+        pass: // generated ethereal password
       },
       // This allows us to run it locally and send off the email to the cleint in local mode
       tls:{
@@ -66,28 +69,39 @@ app.post('/send', (req, res) => {
     });
 
     // send mail with defined transport object
-    let info = transporter.sendMail({
-      from: '', // sender address
+    let mailOptions = ({
+      from: 'freetrailerreminder@gmail.com', // sender address
       to: email, // list of receivers
       subject: "Free TRAIL REMINDER", // Subject line
       text: "Hello world?", // plain text body
       html: output // html body
     });
 
-    // transporter.sendMail(info,(error, info) => {
-    //   if(error) {
-    //     return console.log(error);
-    //   }
-    //   console.log("1")
-    // console.log("Message sent: %s", info.messageId);
-    // console.log("3")
-    // // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    // // Preview only available when sending through an Ethereal account
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // // });
-  
-    
+    // This will send an email every min when app is running
+    //     # ┌────────────── second (optional)
+    //  # │ ┌──────────── minute
+    //  # │ │ ┌────────── hour
+    //  # │ │ │ ┌──────── day of month
+    //  # │ │ │ │ ┌────── month
+    //  # │ │ │ │ │ ┌──── day of week
+    //  # │ │ │ │ │ │
+    //  # │ │ │ │ │ │
+    //  # * * * * * *
+    //https://github.com/node-cron/node-cron
+    cron.schedule('* * * * *', () => {
+      console.log('email sent ever MIN');
+        transporter.sendMail(mailOptions, function(error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email sent: ' + info.response);
+            console.log("Message sent: %s", info.messageId);
+          }
+        });
+    });
     res.render('contact',{layout: false,msg:'Email has been sent'});
   });
-
+  cron.schedule('* * * * *', () => {
+    console.log('running a task every MInitue');
+ });
 app.listen(3000, () => console.log('server started...'));
